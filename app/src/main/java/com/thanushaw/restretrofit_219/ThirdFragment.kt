936d428d2b888835449873a2f30c3d86699ec224
ThanushaW <wijesing_ps17219@stu.kln.ac.lk>
2022-05-10 07:33:32 +0530

    Initial commit

---
+++ app/src/main/java/com/thanushaw/restretrofit_219/ThirdFragment.kt
@@ -0,0 +1,86 @@
+package com.thanushaw.restretrofit_219
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.setFragmentResultListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.thanushaw.restretrofit_219.api.*
+import com.thanushaw.restretrofit_219.databinding.FragmentThirdBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //creating apiInterface instance
+        val apiInterface = RetrofitAPI.create().getComments()
+        //enqueueing apiInterface
+        apiInterface.enqueue( object : Callback<List<Comments>> {
+            @SuppressLint("CheckResult")
+            override fun onResponse(call: Call<List<Comments>>, response: Response<List<Comments>>) {
+                // getting the recyclerview by its id
+                val recyclerview = binding.recyclerview
+                // this creates a vertical layout Manager
+                recyclerview.layoutManager = LinearLayoutManager(parentFragment?.context)
+                //if response body isn't null use it as postList and create the adapter
+                //pass the adapter into recycler view
+                if(response.body() != null){
+                    setFragmentResultListener("requestKey_2") { _, bundle ->
+                        val id = bundle.getInt("id")
+
+                        val data = ArrayList<Comments>()
+                        response.body()!!.forEach{
+
+                            if(it.postId == id){
+                                Log.i("Third Fragment","${it.id} and ${it.postId} and $id")
+                                data.add(Comments(postId = it.postId,id=it.id,name=it.name,email = it.email,body = it.body))
+                            }
+                        }
+                        val adapter = RecyclerViewCustomAdapter2(data)
+                        recyclerview.adapter = adapter
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Comments>>, t: Throwable) {
+                Log.i("Third Fragment","DB Error")
+            }
+        })
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
---
+++ app/src/main/java/com/thanushaw/restretrofit_219/api/Post.kt
@@ -0,0 +1,3 @@
+package com.thanushaw.restretrofit_219.api
+
+data class Post(val userId:Int,val id:Int,val title:String,val body:String)
